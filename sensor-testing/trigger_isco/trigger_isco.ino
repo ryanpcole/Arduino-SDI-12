fad16d37081c5d78f25e30152ab6eedf3b5e9d4d
ryanpcole <ryancole@home>
2023-10-19 12:32:36 -0700

    Thinking about how to trigger ISCO

---
+++ sensor-testing/trigger_isco/trigger_isco.ino
@@ -0,0 +1,23 @@
+/**
+ * @file trigger_isco.ino
+ * @copyright (c) 2013-2020 Stroud Water Research Center (SWRC)
+ *                          and the EnviroDIY Development Team
+ *            This example is published under the BSD-3 license.
+ * @author Ryan Cole
+ * @date 
+ *
+ * @brief Wait a certain amount of time and then trigger an ISCO sample
+ *
+ * This is a simple demonstration of triggering an ISCO using an arduino
+ *
+ * It waits a certain amount of time then triggers the pump to take a sample
+ * 
+ * TODO  I need to figure out exactly what kind of signal it takes to trigger a sample.
+ *       Also is it a specific signal to trigger a sample or do different signals affect
+ *       the programming in the ISCO
+ * 
+ * Or do I have to program the ISCO independently?
+ * 
+ *
+ * 
+ */
